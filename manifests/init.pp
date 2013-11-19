501296fff77ae8263717e44e258bc42f2aec1b36
Abdelouahed Haitoute <ahaitoute@rinis.nl>
2013-11-19 09:37:06 +0100

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,25 @@
+class vyatta (
+  $path = '/config/config.puppet',
+  $host_name = $hostname
+) {
+  concat { $path:
+    owner => root,
+    group => root,
+    mode  => '0644',
+  }
+  concat::fragment { "interfaces_header":
+    target  => "${vyatta::path}",
+    content => template('vyatta/interfaces_header.erb'),
+    order   => 10,
+  }
+  concat::fragment { "interfaces_trailer":
+    target  => "${vyatta::path}",
+    content => template('vyatta/interfaces_trailer.erb'),
+    order   => 19,
+  }
+  concat::fragment { "system_header":
+    target  => "${vyatta::path}",
+    content => template('vyatta/system_header.erb'),
+    order   => 20,
+  }
+}
