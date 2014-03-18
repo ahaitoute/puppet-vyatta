a1cd591d8479cf35a930e1add9d536ff30c94a4f
Abdelouahed Haitoute <ahaitoute@rinis.nl>
2014-03-18 17:16:20 +0100

    Modified vyatta::interfaces::openvpn-resource type based on JSON.pretty_generate.

--- manifests/interfaces/openvpn.pp
+++ manifests/interfaces/openvpn.pp
@@ -1,20 +1,8 @@
 define vyatta::interfaces::openvpn (
-  $ca_cert_file = undef,
-  $cert_file = undef,
   $ensure = present,
-  $key_file = undef,
-  $local_address,
-  $local_port = undef,
-  $mode,
-  $openvpn = $name,
-  $remote_address,
-  $remote_host,
-  $remote_port = undef,
-  $role = undef
+  $configuration,
+  $openvpn = $name
 ) {
-
-#  include vyatta
-
   concat::fragment { "interfaces_${openvpn}":
     target  => "${vyatta::configuration}",
     content => template('vyatta/openvpn.erb'),
