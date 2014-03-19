84eeb63506cdbe9e703fb5f16232aeab0484296a
Abdelouahed Haitoute <ahaitoute@rinis.nl>
2014-03-19 11:34:22 +0100

    Modified vyatta::system::ntp-resource type based on JSON.pretty_generate.

--- manifests/system/ntp.pp
+++ manifests/system/ntp.pp
@@ -1,19 +1,8 @@
 define vyatta::system::ntp (
   $ensure = present,
-  $server = $name,
+  $configuration,
+  $ntp = $name,
 ) {
-  if ! (defined(Concat::Fragment['ntp_header'])) {
-    concat::fragment { 'ntp_header':
-      target  => "${vyatta::configuration}",
-      content => template('vyatta/ntp_header.erb'),
-      order   => 505,
-    }
-    concat::fragment { 'ntp_trailer':
-      target  => "${vyatta::configuration}",
-      content => template('vyatta/ntp_trailer.erb'),
-      order   => 507,
-    }
-  }
   concat::fragment { "ntp_${server}":
     target  => "${vyatta::configuration}",
     content => template('vyatta/ntp.erb'),
